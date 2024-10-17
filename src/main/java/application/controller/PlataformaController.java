9b15c249a2679b7e926df5229baadef5e0baeb98
AmabilieMabi <vitoriaamabiliebotazolimacedom@gmail.com>
2024-10-17 14:22:53 +0000

    games.vivi

--- src/main/java/application/controller/PlataformaController.java
+++ src/main/java/application/controller/PlataformaController.java
@@ -1,6 +1,7 @@
 package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,13 @@ public class PlataformaController {
     @Autowired
     private PlataformaRepository plataformaRepo;
 
+    @GetMapping
+    public Iterable<Plataforma> list(){
+        return plataformaRepo.findAll();
+    }
+
     @PostMapping
-    public Plataforma insert(@RequestBody Plataforma plataforma) {
+    public Plataforma insert(@RequestBody Plataforma plataforma){
         return plataformaRepo.save(plataforma);
     }
-}
+} 
